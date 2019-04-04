bbe50a02f0ca6d7f2dbb838af9312cad423109b8
xx <xin7c@qq.com>
2019-04-04 18:07:45 +0800

    home

---
+++ xuchu/schema.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User as UserModel
+from graphene_django import DjangoObjectType
+import graphene
+
+class User(DjangoObjectType):
+    class Meta:
+        model = UserModel
+
+class Hero(graphene.ObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+
+
+class Query(graphene.ObjectType):
+    users = graphene.List(User)
+    hero = graphene.Field(Hero)
+
+    @graphene.resolve_only_args
+    def resolve_users(self):
+        return UserModel.objects.all()
+    def resolve_hero(self, info):
+        return Hero(id=1, name="xuchu")
+schema = graphene.Schema(query=Query)
